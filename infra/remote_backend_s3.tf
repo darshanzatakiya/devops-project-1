f7fada7e96ad75c62aef9ece5e6d2ddf0f42d029
Darshan Zatakiya <darshanzatakiya13@gmail.com>
2025-07-06 19:30:29 +0530

    11

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-1122"
+    bucket = "dev-proj-1-remote-state-bucket-1122-hj"
     key    = "devops-project-1-main/Jenkinsfile/terraform.tfstate"
-    region = "eu-west-1"
+    region = "eu-central-1"
   }
 }
